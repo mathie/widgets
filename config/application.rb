7df5d418b080985190b27e80d9e58855cbf90ac0
Graeme Mathieson <mathie@woss.name>
2014-11-19 14:40:59 +0000

    Don't give the app the same name as a model.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ end
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Widget
+module Widgets
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
   end
